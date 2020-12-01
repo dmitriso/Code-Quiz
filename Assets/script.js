c19a84ad74296ad729a2214c625c3e050f669386
Dmitri So <dmitriso@yahoo.com>
2020-12-01 16:41:40 -0500

    HTML & Javascript Psuedo Code

---
+++ Assets/script.js
@@ -0,0 +1,7 @@
+// Must create logic for HTML here
+// A timed quiz on Javascript basics that store high scores
+// When user clicks start button timer starts
+// user is prompted with quiz questions
+// A tracker to store high scores
+// Displayed timer counting remaining time
+// 
