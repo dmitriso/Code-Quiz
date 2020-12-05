ea5a6122945b0bb7ece833ccf876a1238960e6c6
Dmitri So <dmitriso@yahoo.com>
2020-12-04 21:22:25 -0500

    Created function that initiates the end of quiz

--- Assets/script.js
+++ Assets/script.js
@@ -5,48 +5,55 @@ var quizEl = document.getElementById("quiz");
 var questionNumber = document.getElementById("questionNum")
 var questionEl = document.getElementById("question");
 var startButton = document.getElementById("submit");
+var userSubmit = document.getElementById("userSubmit")
 var score = 0;
 var i = 0;
-var timeLeft = 125;
+var timeLeft = 50;
 // Creates Choice Elements (buttons)
-var allChoices = document.createElement("ul");
-var choice1 = document.createElement("button");
-var choice2 = document.createElement("button");
-var choice3 = document.createElement("button");
-var choice4 = document.createElement("button");
-var choiceBtn = document.getElementById("choices");
+var allChoices = document.getElementById("choices");
+var choice1 = document.getElementById("button1");
+var choice2 = document.getElementById("button2");
+var choice3 = document.getElementById("button3");
+var choice4 = document.getElementById("button4");
 
 
 
 // Arrays containing all questions and corresponding answers
 var questions = [
         {
-                number: 1,
+
                 // Questions
                 question: "Inside which element do you put JavaScript?",
-                // Choices
+                // Array storing all choices
                 choices: ["<var>", "<script>", "<section>", "<code>"],
                 // Answer
                 correct: "<script>",
         },
         {
-                number: 2,
+
                 question: "Which is the correct way to write a comment in JavaScript?",
                 choices: ["{# ... #}", "<!--- ... ---!>", "// ...", "\\ ..."],
                 correct: "// ...",
         },
         {
-                number: 3,
+
                 question: "Which of the following is used to identify the array?",
                 choices: [" ===", "typeof", "isarrayType()", "=="],
                 correct: "typeof",
         },
         {
-                number: 4,
+
                 question: "Which one of the following is correct?",
                 choices: ["i =+ 1;", "i += 1;", "i = i++1;", "+i+;"],
                 correct: "i += 1;",
+        },
+        {
+                question: "",
+                choices: [],
+                correct: "",
+
         }
+
 ];
 // when user clicks button quiz starts and timer starts
 document.getElementById("startBtn").addEventListener("click", startGame);
@@ -57,6 +64,7 @@ function startGame() {
         // reveals quiz
         document.getElementById("quiz").classList.remove("hidden");
         // displays choices
+        allChoices.removeAttribute("style", "display:none");
         choice1.removeAttribute("style", "display:none");
         choice2.removeAttribute("style", "display:none");
         choice3.removeAttribute("style", "display:none");
@@ -68,59 +76,56 @@ function startGame() {
 
 // Timing counter function
 function counter() {
-        
+
         var timer = setInterval(function () {
                 timerEl.textContent = "Time:" + timeLeft;
                 timeLeft--;
                 // console.log(timeLeft);
+                // If time runs out before quiz is done then the test stops
                 if (timeLeft === 0) {
                         timerEl.textContent = "";
                         clearInterval(timer);
+                        choice1.setAttribute("style", "display:none");
+                        choice2.setAttribute("style", "display:none");
+                        choice3.setAttribute("style", "display:none");
+                        choice4.setAttribute("style", "display:none");
+                        allChoices.setAttribute("style", "display:none");
+                        document.getElementById("quiz").classList.add("hidden");
+                        document.getElementById("endQuiz").classList.remove("hidden");
+                        document.getElementById("timer").classList.add("hidden");
                 }
         }, 1000);
 };
 
 // Generate a questions by creating an element
 function populate() {
-        
+
+        // if time allows try to refactor this code with a loop
         questionEl.textContent = questions[i].question;
-        // First question
+
         choice1.textContent = questions[i].choices[0];
         choice1.setAttribute("value", questions[i].choices[0]);
         choice1.addEventListener("click", checkAnswer);
-        // Second question
+
         choice2.textContent = questions[i].choices[1];
         choice2.setAttribute("value", questions[i].choices[1]);
         choice2.addEventListener("click", checkAnswer);
-        // Third question
+
         choice3.textContent = questions[i].choices[2];
         choice3.setAttribute("value", questions[i].choices[2]);
         choice3.addEventListener("click", checkAnswer);
-        // Fourth question
+
         choice4.textContent = questions[i].choices[3];
         choice4.setAttribute("value", questions[i].choices[3]);
         choice4.addEventListener("click", checkAnswer);
+
 };
 populate();
-// using set attribute to hide the previous content
-// Appending children to elements
-if (choiceBtn) {
-        choiceBtn.appendChild(allChoices);
-        allChoices.appendChild(choice1);
-        allChoices.appendChild(choice2);
-        allChoices.appendChild(choice3);
-        allChoices.appendChild(choice4);
-}
-if (choices) {
-        choice1.setAttribute("style", "display:none");
-        choice2.setAttribute("style", "display:none");
-        choice3.setAttribute("style", "display:none");
-        choice4.setAttribute("style", "display:none");
-}
-// Function that chekcs if answer is correct
+// Function that checks if answer is correct
 function checkAnswer() {
-        var  correctAnswer = questions[i].correct;
+        var correctAnswer = questions[i].correct;
         if (this.value === correctAnswer) {
+                score = timeLeft;
                 timeLeft += 10;
                 console.log(score);
         } else {
@@ -129,5 +134,38 @@ function checkAnswer() {
         }
         i++;
         populate();
+        endGame();
 }
-// document.getElementById("choiceBtn").classList.add(btn,btn-success);
+
+// A fucntion that ends the game when all questions have been answered
+function endGame() {
+
+        if (i === 4) {
+                timer.textContent = "";
+                clearInterval(timer);
+                choice1.setAttribute("style", "display:none");
+                choice2.setAttribute("style", "display:none");
+                choice3.setAttribute("style", "display:none");
+                choice4.setAttribute("style", "display:none");
+                allChoices.setAttribute("style", "display:none");
+                document.getElementById("quiz").classList.add("hidden");
+                document.getElementById("endQuiz").classList.remove("hidden");
+                document.getElementById("timer").classList.add("hidden");
+
+        }
+}
+
+
+// When user clicks submit button at end of quiz then user information is stored in local data
+// document.getElementById("userSubmit").addEventListener("click", function (event) {
+//         event.preventDefault();
+//         var userName = document.getElementById("userName").value.trim();
+//         var user = {
+//                 Name: userName,
+//                 score: score,
+//         };
+
+//         localStorage.setItem("userData", JSON.stringify(user))
+
+// });
+
